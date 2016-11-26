55f1866b7eb23738c401a51b0757c28530838de3
techgaun <coolsamar207@gmail.com>
2016-11-26 13:51:32 -0600

    add initial version of sum of multiples

--- elixir/sum-of-multiples/sum_of_multiples.exs
+++ elixir/sum-of-multiples/sum_of_multiples.exs
@@ -4,6 +4,23 @@ defmodule SumOfMultiples do
   """
   @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
   def to(limit, factors) do
+    limit
+    |> Kernel.-(1)
+    |> sum(factors, 0)
+  end
+  def sum(v, _factors, sum) when v < 1, do: sum
+  def sum(v, factors, sum) do
+    sum = if factor?(v, factors, false) do
+      sum + v
+    else
+      sum
+    end
+    sum(v - 1, factors, sum)
+  end
 
+  def factor?(_, _, true), do: true
+  def factor?(_v, [], is_factor), do: is_factor
+  def factor?(v, [h | t], _is_factor) do
+    factor?(v, t, rem(v, h) === 0)
   end
 end
