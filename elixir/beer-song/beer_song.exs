860b5efc781c6c03ae9972407dd830fd7b6f1182
techgaun <coolsamar207@gmail.com>
2016-11-27 01:38:05 -0600

    simplify code little bit

--- elixir/beer-song/beer_song.exs
+++ elixir/beer-song/beer_song.exs
@@ -11,12 +11,16 @@ defmodule BeerSong do
     """
   end
 
-  defp verse_params(1), do: {"no more bottles", "Go to the store and buy some more", "99 bottles"}
-  defp verse_params(2), do: {"1 bottle", "Take it down and pass it around", "no more bottles"}
-  defp verse_params(num), do: {"#{bottles(num - 1)}", "Take one down and pass it around", "#{bottles(num - 2)}"}
-  defp bottles(0), do: "no more bottles"
-  defp bottles(1), do: "1 bottle"
-  defp bottles(n), do: "#{n} bottles"
+  defp verse_params(num), do: {"#{bottles(num)}", perform(num), "#{bottles(num - 1)}"}
+
+  defp bottles(0), do: bottles(100)
+  defp bottles(1), do: "no more bottles"
+  defp bottles(2), do: "1 bottle"
+  defp bottles(n), do: "#{n - 1} bottles"
+
+  defp perform(1), do: "Go to the store and buy some more"
+  defp perform(2), do: "Take it down and pass it around"
+  defp perform(_), do: "Take one down and pass it around"
 
   @doc """
   Get the entire beer song for a given range of numbers of bottles.
@@ -24,7 +28,7 @@ defmodule BeerSong do
   @spec lyrics(Range.t) :: String.t
   def lyrics(range \\ 100..1) do
     range
-    |> Enum.map(&(verse(&1)))
+    |> Stream.map(&verse/1)
     |> Enum.join("\n")
   end
 end
